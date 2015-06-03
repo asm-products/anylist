ec43c643d75bf16823f0891e009f5f538c53fbf2
Sergey Novikov <novikov359@gmail.com>
2015-06-03 19:59:45 +0300

    Capified

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+lock '3.4.0'
+
+set :application, 'anylist'
+set :repo_url, 'git@github.com:droptheplot/anylist.git'
+
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+set :deploy_to, '/var/www/anylist'
+
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/uploads')
+
+namespace :deploy do
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      within release_path do
+        with rails_env: :production do
+          execute :rake, 'assets:precompile'
+        end
+      end
+    end
+  end
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,1 @@
+server '46.101.141.168', user: 'deploy', roles: %w{app db web}, :primary => true
